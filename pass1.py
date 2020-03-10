e21f4f982f8af63b0a57ee55b2015b7cabff5e4e
Omkar <omkarchorghe22@gmail.com>
2020-03-10 21:02:52 +0530

    Pass 1 basic logic

--- pass1.py
+++ pass1.py
@@ -6,3 +6,15 @@ POT = readfile("POT.txt")
 pr.pprint(MOT)
 pr.pprint(POT)
 
+if os.path.exists("sourcecode.txt"):
+    f = open("sourcecode.txt","r")
+    lines = f.readlines()
+    for instr in lines:
+        instr = instr.strip()
+        words = instr.split(" ")
+        if words[0] in [entry["Mnemonic"] for entry in POT]:
+            print("POT")
+        elif words[0] in [ entry["Mnemonic"] for entry in MOT]:
+            print("MOT")
+        else:
+            print("label")
