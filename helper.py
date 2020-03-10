983baef3deee965aa1ae6fb3b3642fe0002d398a
fork52 <mrunankmistry52@gmail.com>
2020-03-10 23:53:05 +0530

    INSTRUCTION GENERATOR CREATED

--- helper.py
+++ helper.py
@@ -1,20 +1,18 @@
 import os
 
-
 def readfile(filename):
     ''' reads the file and returns it as list of dictionaries'''
     if os.path.exists(filename):
         f = open(filename,"r")
         lines = f.readlines()
+        lines = [ i.strip() for i in lines]
         data = []
 
         #first line is headers
-        headers = lines[0].strip()
-        headers = headers.split(',')
-
+        headers = lines[0].split(',')
+        
         for line in lines[1:]:
-            line1 = line.strip()
-            temp = line1.split(',')
+            temp = line.split(',')
             data.append( { headers[i]: temp[i] for i in range(len(headers)) } )
 
         return data
