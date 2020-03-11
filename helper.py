194dd0b23d1e83172d6fc8b2c40be19cd04d8867
Omkar <omkarchorghe22@gmail.com>
2020-03-11 18:29:12 +0530

    Phase 1 80%

--- helper.py
+++ helper.py
@@ -5,14 +5,15 @@ def readfile(filename):
     if os.path.exists(filename):
         f = open(filename,"r")
         lines = f.readlines()
-        lines = [ i.strip() for i in lines]
         data = []
 
         #first line is headers
-        headers = lines[0].split(',')
-        
+        headers = lines[0].strip()
+        headers = headers.split(':')
+
         for line in lines[1:]:
-            temp = line.split(',')
+            line1 = line.strip()
+            temp = line1.split(':')
             data.append( { headers[i]: temp[i] for i in range(len(headers)) } )
 
         return data
